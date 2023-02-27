869f804c9d670afc761e18fd36ca051bc3955372
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2023-02-28 00:40:38 +0100

    move f_declarations

--- src/mg/common/Types.h
+++ src/mg/common/Types.h
@@ -2,18 +2,6 @@
 
 #include "mg/common/Definitions.h"
 
-#define F_DECLARE_CLASS(NAMESPACE_1, NAMESPACE_2, CLASS_TYPE) \
-	namespace NAMESPACE_1 { namespace NAMESPACE_2 { class CLASS_TYPE; } }
-
-#define F_DECLARE_CLASS_3(NAMESPACE_1, NAMESPACE_2, NAMESPACE_3, CLASS_TYPE) \
-	namespace NAMESPACE_1 { namespace NAMESPACE_2 { namespace NAMESPACE_3 { class CLASS_TYPE; } } }
-
-#define F_DECLARE_STRUCT(NAMESPACE_1, NAMESPACE_2, STRUCT_TYPE) \
-	namespace NAMESPACE_1 { namespace NAMESPACE_2 { struct STRUCT_TYPE; } }
-
-#define F_DECLARE_STRUCT_3(NAMESPACE_1, NAMESPACE_2, NAMESPACE_3, STRUCT_TYPE) \
-	namespace NAMESPACE_1 { namespace NAMESPACE_2 { namespace NAMESPACE_3 { struct STRUCT_TYPE; } } }
-
 #include <climits>
 #include <cmath>
 #include <cstddef>
