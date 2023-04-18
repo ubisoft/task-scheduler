a44240221ca5a1e25f8f31da810c0239fc75e1d4
Vladislav Shpilevoy <v.shpilevoy@tarantool.org>
2023-04-18 23:46:31 +0200

    atomic: introduce Atomic class
    It is a wrapper around std::atomic without any operators. The
    problem of std::atomic is that it has expensive implicit operators
    which are easy to miss. Also it lacks useful methods like
    increment with custom memory order, add_fetch, and some others.
    
    Part of #16

--- src/mg/common/Atomic.h
+++ src/mg/common/Atomic.h
@@ -7,3 +7,339 @@
 #else
 #include "Atomic_Win.h"
 #endif
+
+#include <atomic>
+
+namespace mg {
+namespace common {
+
+	template<typename T>
+	class Atomic
+	{
+	public:
+		Atomic() = default;
+		Atomic(const T& aValue) : myValue(aValue) {}
+
+		T LoadRelaxed() const
+		{ return myValue.load(std::memory_order_relaxed); }
+		T LoadAcquire() const
+		{ return myValue.load(std::memory_order_acquire); }
+		T Load() const
+		{ return myValue.load(std::memory_order_seq_cst); }
+
+		void StoreRelaxed(
+			const T& aValue)
+		{ return myValue.store(aValue, std::memory_order_relaxed); }
+		void StoreRelease(
+			const T& aValue)
+		{ return myValue.store(aValue, std::memory_order_release); }
+		void Store(
+			const T& aValue)
+		{ return myValue.store(aValue, std::memory_order_seq_cst); }
+
+		T ExchangeRelaxed(
+			const T& aValue)
+		{ return myValue.exchange(aValue, std::memory_order_relaxed); }
+		T ExchangeAcqRel(
+			const T& aValue)
+		{ return myValue.exchange(aValue, std::memory_order_acq_rel); }
+		T ExchangeRelease(
+			const T& aValue)
+		{ return myValue.exchange(aValue, std::memory_order_release); }
+		T Exchange(
+			const T& aValue)
+		{ return myValue.exchange(aValue, std::memory_order_seq_cst); }
+
+		T FetchAddRelaxed(
+			const T& aValue)
+		{ return myValue.fetch_add(aValue, std::memory_order_relaxed); }
+		T FetchAddAcqRel(
+			const T& aValue)
+		{ return myValue.fetch_add(aValue, std::memory_order_acq_rel); }
+		T FetchAddRelease(
+			const T& aValue)
+		{ return myValue.fetch_add(aValue, std::memory_order_release); }
+		T FetchAdd(
+			const T& aValue)
+		{ return myValue.fetch_add(aValue, std::memory_order_seq_cst); }
+
+		T AddFetchRelaxed(
+			const T& aValue)
+		{ return myValue.fetch_add(aValue, std::memory_order_relaxed) + aValue; }
+		T AddFetchAcqRel(
+			const T& aValue)
+		{ return myValue.fetch_add(aValue, std::memory_order_acq_rel) + aValue; }
+		T AddFetchRelease(
+			const T& aValue)
+		{ return myValue.fetch_add(aValue, std::memory_order_release) + aValue; }
+		T AddFetch(
+			const T& aValue)
+		{ return myValue.fetch_add(aValue, std::memory_order_seq_cst) + aValue; }
+
+		void AddRelaxed(
+			const T& aValue)
+		{ myValue.fetch_add(aValue, std::memory_order_relaxed) + aValue; }
+		void AddAcqRel(
+			const T& aValue)
+		{ myValue.fetch_add(aValue, std::memory_order_acq_rel) + aValue; }
+		void AddRelease(
+			const T& aValue)
+		{ myValue.fetch_add(aValue, std::memory_order_release) + aValue; }
+		void Add(
+			const T& aValue)
+		{ myValue.fetch_add(aValue, std::memory_order_seq_cst) + aValue; }
+
+		T FetchSubRelaxed(
+			const T& aValue)
+		{ return myValue.fetch_sub(aValue, std::memory_order_relaxed); }
+		T FetchSubAcqRel(
+			const T& aValue)
+		{ return myValue.fetch_sub(aValue, std::memory_order_acq_rel); }
+		T FetchSubRelease(
+			const T& aValue)
+		{ return myValue.fetch_sub(aValue, std::memory_order_release); }
+		T FetchSub(
+			const T& aValue)
+		{ return myValue.fetch_sub(aValue, std::memory_order_seq_cst); }
+
+		T SubFetchRelaxed(
+			const T& aValue)
+		{ return myValue.fetch_sub(aValue, std::memory_order_relaxed) - aValue; }
+		T SubFetchAcqRel(
+			const T& aValue)
+		{ return myValue.fetch_sub(aValue, std::memory_order_acq_rel) - aValue; }
+		T SubFetchRelease(
+			const T& aValue)
+		{ return myValue.fetch_sub(aValue, std::memory_order_release) - aValue; }
+		T SubFetch(
+			const T& aValue)
+		{ return myValue.fetch_sub(aValue, std::memory_order_seq_cst) - aValue; }
+
+		void SubRelaxed(
+			const T& aValue)
+		{ myValue.fetch_sub(aValue, std::memory_order_relaxed); }
+		void SubAcqRel(
+			const T& aValue)
+		{ myValue.fetch_sub(aValue, std::memory_order_acq_rel); }
+		void SubRelease(
+			const T& aValue)
+		{ myValue.fetch_sub(aValue, std::memory_order_release); }
+		void Sub(
+			const T& aValue)
+		{ myValue.fetch_sub(aValue, std::memory_order_seq_cst); }
+
+		void IncrementRelaxed()
+		{ myValue.fetch_add(1, std::memory_order_relaxed); }
+		void IncrementAcqRel()
+		{ myValue.fetch_add(1, std::memory_order_acq_rel); }
+		void IncrementRelease()
+		{ myValue.fetch_add(1, std::memory_order_release); }
+		void Increment()
+		{ myValue.fetch_add(1, std::memory_order_seq_cst); }
+
+		T FetchIncrementRelaxed()
+		{ return myValue.fetch_add(1, std::memory_order_relaxed); }
+		T FetchIncrementAcqRel()
+		{ return myValue.fetch_add(1, std::memory_order_acq_rel); }
+		T FetchIncrementRelease()
+		{ return myValue.fetch_add(1, std::memory_order_release); }
+		T FetchIncrement()
+		{ return myValue.fetch_add(1, std::memory_order_seq_cst); }
+
+		T IncrementFetchRelaxed()
+		{ return myValue.fetch_add(1, std::memory_order_relaxed) + 1; }
+		T IncrementFetchAcqRel()
+		{ return myValue.fetch_add(1, std::memory_order_acq_rel) + 1; }
+		T IncrementFetchRelease()
+		{ return myValue.fetch_add(1, std::memory_order_release) + 1; }
+		T IncrementFetch()
+		{ return myValue.fetch_add(1, std::memory_order_seq_cst) + 1; }
+
+		void DecrementRelaxed()
+		{ myValue.fetch_sub(1, std::memory_order_relaxed); }
+		void DecrementAcqRel()
+		{ myValue.fetch_sub(1, std::memory_order_acq_rel); }
+		void DecrementRelease()
+		{ myValue.fetch_sub(1, std::memory_order_release); }
+		void Decrement()
+		{ myValue.fetch_sub(1, std::memory_order_seq_cst); }
+
+		T FetchDecrementRelaxed()
+		{ return myValue.fetch_sub(1, std::memory_order_relaxed); }
+		T FetchDecrementAcqRel()
+		{ return myValue.fetch_sub(1, std::memory_order_acq_rel); }
+		T FetchDecrementRelease()
+		{ return myValue.fetch_sub(1, std::memory_order_release); }
+		T FetchDecrement()
+		{ return myValue.fetch_sub(1, std::memory_order_seq_cst); }
+
+		T DecrementFetchRelaxed()
+		{ return myValue.fetch_sub(1, std::memory_order_relaxed) - 1; }
+		T DecrementFetchAcqRel()
+		{ return myValue.fetch_sub(1, std::memory_order_acq_rel) - 1; }
+		T DecrementFetchRelease()
+		{ return myValue.fetch_sub(1, std::memory_order_release) - 1; }
+		T DecrementFetch()
+		{ return myValue.fetch_sub(1, std::memory_order_seq_cst) - 1; }
+
+		bool CmpExchgWeakRelaxed(
+			T& aExpected,
+			const T& aValue);
+		bool CmpExchgWeakAcquire(
+			T& aExpected,
+			const T& aValue);
+		bool CmpExchgWeakAcqRel(
+			T& aExpected,
+			const T& aValue);
+		bool CmpExchgWeakRelease(
+			T& aExpected,
+			const T& aValue);
+		bool CmpExchgWeak(
+			T& aExpected,
+			const T& aValue);
+
+		bool CmpExchgStrongRelaxed(
+			T& aExpected,
+			const T& aValue);
+		bool CmpExchgStrongAcquire(
+			T& aExpected,
+			const T& aValue);
+		bool CmpExchgStrongAcqRel(
+			T& aExpected,
+			const T& aValue);
+		bool CmpExchgStrongRelease(
+			T& aExpected,
+			const T& aValue);
+		bool CmpExchgStrong(
+			T& aExpected,
+			const T& aValue);
+
+	private:
+		std::atomic<T> myValue;
+	};
+
+	using AtomicU8 = Atomic<uint8_t>;
+	using AtomicU16 = Atomic<uint16_t>;
+	using AtomicU32 = Atomic<uint32_t>;
+	using AtomicU64 = Atomic<uint64_t>;
+	using AtomicI8 = Atomic<uint8_t>;
+	using AtomicI16 = Atomic<int16_t>;
+	using AtomicI32 = Atomic<int32_t>;
+	using AtomicI64 = Atomic<int64_t>;
+	using AtomicBool = Atomic<bool>;
+	using AtomicVoidPtr = Atomic<void*>;
+
+	////////////////////////////////////////////////////////////////////////
+
+	template<typename T>
+	inline bool
+	Atomic<T>::CmpExchgWeakRelaxed(
+		T& aExpected,
+		const T& aValue)
+	{
+		return myValue.compare_exchange_weak(aExpected, aValue,
+			std::memory_order_relaxed,
+			std::memory_order_relaxed);
+	}
+
+	template<typename T>
+	inline bool
+	Atomic<T>::CmpExchgWeakAcquire(
+		T& aExpected,
+		const T& aValue)
+	{
+		return myValue.compare_exchange_weak(aExpected, aValue,
+			std::memory_order_acquire,
+			std::memory_order_relaxed);
+	}
+
+	template<typename T>
+	inline bool
+	Atomic<T>::CmpExchgWeakAcqRel(
+		T& aExpected,
+		const T& aValue)
+	{
+		return myValue.compare_exchange_weak(aExpected, aValue,
+			std::memory_order_acq_rel,
+			std::memory_order_relaxed);
+	}
+
+	template<typename T>
+	inline bool
+	Atomic<T>::CmpExchgWeakRelease(
+		T& aExpected,
+		const T& aValue)
+	{
+		return myValue.compare_exchange_weak(aExpected, aValue,
+			std::memory_order_release,
+			std::memory_order_relaxed);
+	}
+
+	template<typename T>
+	inline bool
+	Atomic<T>::CmpExchgWeak(
+		T& aExpected,
+		const T& aValue)
+	{
+		return myValue.compare_exchange_weak(aExpected, aValue,
+			std::memory_order_seq_cst,
+			std::memory_order_relaxed);
+	}
+
+	template<typename T>
+	inline bool
+	Atomic<T>::CmpExchgStrongRelaxed(
+		T& aExpected,
+		const T& aValue)
+	{
+		return myValue.compare_exchange_strong(aExpected,
+			aValue, std::memory_order_relaxed,
+			std::memory_order_relaxed);
+	}
+
+	template<typename T>
+	inline bool
+	Atomic<T>::CmpExchgStrongAcquire(
+		T& aExpected,
+		const T& aValue)
+	{
+		return myValue.compare_exchange_strong(aExpected,
+			aValue, std::memory_order_acquire,
+			std::memory_order_relaxed);
+	}
+
+	template<typename T>
+	inline bool
+	Atomic<T>::CmpExchgStrongAcqRel(
+		T& aExpected,
+		const T& aValue)
+	{
+		return myValue.compare_exchange_strong(aExpected,
+			aValue, std::memory_order_acq_rel,
+			std::memory_order_relaxed);
+	}
+
+	template<typename T>
+	inline bool
+	Atomic<T>::CmpExchgStrongRelease(
+		T& aExpected,
+		const T& aValue)
+	{
+		return myValue.compare_exchange_strong(aExpected,
+			aValue, std::memory_order_release,
+			std::memory_order_relaxed);
+	}
+
+	template<typename T>
+	inline bool
+	Atomic<T>::CmpExchgStrong(
+		T& aExpected,
+		const T& aValue)
+	{
+		return myValue.compare_exchange_strong(aExpected,
+			aValue, std::memory_order_seq_cst,
+			std::memory_order_relaxed);
+	}
+
+}
+}
--- test/UnitTestAtomic.cpp
+++ test/UnitTestAtomic.cpp
@@ -3,130 +3,357 @@
 
 #include "UnitTest.h"
 
+#define TEST_CHECK MG_COMMON_ASSERT
+
 namespace mg {
 namespace unittests {
 
 	static void
-	UnitTestAtomicIncrement()
+	UnitTestAtomicConstructor()
 	{
-		int32_t vi32 = 5;
-		MG_COMMON_ASSERT(mg::common::AtomicIncrement(&vi32) == 6);
-		MG_COMMON_ASSERT(vi32 == 6);
-
-		int64_t vi64 = (uint64_t)UINT32_MAX;
-		MG_COMMON_ASSERT(mg::common::AtomicIncrement64(&vi64) == (int64_t)UINT32_MAX + 1);
-		MG_COMMON_ASSERT(vi64 == (int64_t)UINT32_MAX + 1);
-	}
-
-	static void
-	UnitTestAtomicDecrement()
-	{
-		int32_t vi32 = 5;
-		MG_COMMON_ASSERT(mg::common::AtomicDecrement(&vi32) == 4);
-		MG_COMMON_ASSERT(vi32 == 4);
-	}
-
-	static void
-	UnitTestAtomicExchange()
-	{
-		int32_t vi32 = 5;
-		MG_COMMON_ASSERT(mg::common::AtomicExchange(&vi32, 10) == 5);
-		MG_COMMON_ASSERT(vi32 == 10);
-
-		uint32_t vu32 = 5;
-		MG_COMMON_ASSERT(mg::common::AtomicExchangeU32(&vu32, 10) == 5);
-		MG_COMMON_ASSERT(vu32 == 10);
-
-		int64_t vi64 = (int64_t)UINT32_MAX + 1;
-		MG_COMMON_ASSERT(mg::common::AtomicExchange64(&vi64, (int64_t)UINT32_MAX + 10) ==
-			(int64_t)UINT32_MAX + 1);
-		MG_COMMON_ASSERT(vi64 == (int64_t)UINT32_MAX + 10);
-
-		uint64_t vu64 = (uint64_t)UINT32_MAX + 1;
-		MG_COMMON_ASSERT(mg::common::AtomicExchangeU64(&vu64, (uint64_t)UINT32_MAX + 10) ==
-			(uint64_t)UINT32_MAX + 1);
-		MG_COMMON_ASSERT(vu64 == (uint64_t)UINT32_MAX + 10);
-
-		void* vptr = &vu64;
-		MG_COMMON_ASSERT(mg::common::AtomicExchangePtr(&vptr, (void*)&vi64) ==
-			(void*)&vu64);
-		MG_COMMON_ASSERT(vptr == (void*)&vi64);
-	}
-
-	static void
-	UnitTestAtomicCompareExchange()
-	{
-		int32_t vi32 = 5;
-		MG_COMMON_ASSERT(mg::common::AtomicCompareExchange(&vi32, 10, 6) == 5);
-		MG_COMMON_ASSERT(vi32 == 5);
-		MG_COMMON_ASSERT(mg::common::AtomicCompareExchange(&vi32, 10, 5) == 5);
-		MG_COMMON_ASSERT(vi32 == 10);
-
-		int64_t vi64 = (int64_t)UINT32_MAX + 1;
-		MG_COMMON_ASSERT(mg::common::AtomicCompareExchange64(&vi64, (int64_t)UINT32_MAX + 10,
-			(int64_t)UINT32_MAX + 5) == (int64_t)UINT32_MAX + 1);
-		MG_COMMON_ASSERT(vi64 == (int64_t)UINT32_MAX + 1);
-		MG_COMMON_ASSERT(mg::common::AtomicCompareExchange64(&vi64, (int64_t)UINT32_MAX + 10,
-			(int64_t)UINT32_MAX + 1) == (int64_t)UINT32_MAX + 1);
-		MG_COMMON_ASSERT(vi64 == (int64_t)UINT32_MAX + 10);
-
-		int32_t dummyVal;
-		void* vptr = &vi64;
-		MG_COMMON_ASSERT(mg::common::AtomicCompareExchangePtr(&vptr, (void*)&vi32,
-			(void*)&dummyVal) == (void*)&vi64);
-		MG_COMMON_ASSERT(vptr == (void*)&vi64);
-		MG_COMMON_ASSERT(mg::common::AtomicCompareExchangePtr(&vptr, (void*)&vi32,
-			(void*)&vi64) == (void*)&vi64);
-		MG_COMMON_ASSERT(vptr == (void*)&vi32);
-	}
-
-	static void
-	UnitTestAtomicAdd()
-	{
-		int32_t vi32 = 1;
-		MG_COMMON_ASSERT(mg::common::AtomicAdd(&vi32, 2) == 1);
-		MG_COMMON_ASSERT(vi32 == 3);
-
-		uint32_t vu32 = 1;
-		MG_COMMON_ASSERT(mg::common::AtomicAddU32(&vu32, 2) == 1);
-		MG_COMMON_ASSERT(vu32 == 3);
-
-		int64_t vi64 = (int64_t)UINT32_MAX + 1;
-		MG_COMMON_ASSERT(mg::common::AtomicAdd64(&vi64, 2) == (int64_t)UINT32_MAX + 1);
-		MG_COMMON_ASSERT(vi64 == (int64_t)UINT32_MAX + 3);
-	}
-
-	static void
-	UnitTestAtomicFlag()
-	{
-		int32_t flag = 0;
-		MG_COMMON_ASSERT(mg::common::AtomicFlagTest(&flag) == 0);
-		MG_COMMON_ASSERT(mg::common::AtomicFlagClear(&flag) == 0);
-		MG_COMMON_ASSERT(flag == 0);
-
-		MG_COMMON_ASSERT(mg::common::AtomicFlagSet(&flag) == 0);
-		MG_COMMON_ASSERT(flag == 1);
-		MG_COMMON_ASSERT(mg::common::AtomicFlagTest(&flag) == 1);
-		MG_COMMON_ASSERT(mg::common::AtomicFlagSet(&flag) == 1);
-		MG_COMMON_ASSERT(flag == 1);
-
-		MG_COMMON_ASSERT(mg::common::AtomicFlagClear(&flag) == 1);
-		MG_COMMON_ASSERT(flag == 0);
+		// Default.
+		{
+			mg::common::Atomic<int> value;
+			value.Store(123);
+			TEST_CHECK(value.Load() == 123);
+		}
+		// With a value
+		{
+			mg::common::Atomic<int> value(123);
+			TEST_CHECK(value.Load() == 123);
+		}
 	}
 
 	static void
 	UnitTestAtomicLoad()
 	{
-		int32_t vi32 = 100;
-		MG_COMMON_ASSERT(mg::common::AtomicLoad(&vi32) == 100);
-		uint32_t vu32 = 200;
-		MG_COMMON_ASSERT(mg::common::AtomicLoadU32(&vu32) == 200);
-		int64_t vi64 = 300;
-		MG_COMMON_ASSERT(mg::common::AtomicLoad64(&vi64) == 300);
-		uint64_t vu64 = 400;
-		MG_COMMON_ASSERT(mg::common::AtomicLoadU64(&vu64) == 400);
-		void* vptr = &vu64;
-		MG_COMMON_ASSERT(mg::common::AtomicLoadPtr(&vptr) == (void*)&vu64);
+		mg::common::Atomic<int> value(123);
+		TEST_CHECK(value.LoadRelaxed() == 123);
+		TEST_CHECK(value.LoadAcquire() == 123);
+		TEST_CHECK(value.Load() == 123);
+	}
+
+	static void
+	UnitTestAtomicStore()
+	{
+		mg::common::Atomic<int> value(123);
+		value.StoreRelaxed(4);
+		TEST_CHECK(value.LoadRelaxed() == 4);
+		value.StoreRelease(5);
+		TEST_CHECK(value.LoadRelaxed() == 5);
+		value.Store(6);
+		TEST_CHECK(value.LoadRelaxed() == 6);
+	}
+
+	static void
+	UnitTestAtomicExchange()
+	{
+		mg::common::Atomic<int> value(123);
+		TEST_CHECK(value.ExchangeRelaxed(4) == 123);
+		TEST_CHECK(value.ExchangeAcqRel(5) == 4);
+		TEST_CHECK(value.ExchangeRelease(6) == 5);
+		TEST_CHECK(value.Exchange(7) == 6);
+		TEST_CHECK(value.LoadRelaxed() == 7);
+	}
+
+	static void
+	UnitTestAtomicFetchAdd()
+	{
+		mg::common::Atomic<int> value(123);
+		TEST_CHECK(value.FetchAddRelaxed(2) == 123);
+		TEST_CHECK(value.FetchAddAcqRel(3) == 125);
+		TEST_CHECK(value.FetchAddRelease(4) == 128);
+		TEST_CHECK(value.FetchAdd(5) == 132);
+		TEST_CHECK(value.LoadRelaxed() == 137);
+	}
+
+	static void
+	UnitTestAtomicAddFetch()
+	{
+		mg::common::Atomic<int> value(123);
+		TEST_CHECK(value.AddFetchRelaxed(2) == 125);
+		TEST_CHECK(value.AddFetchAcqRel(3) == 128);
+		TEST_CHECK(value.AddFetchRelease(4) == 132);
+		TEST_CHECK(value.AddFetch(5) == 137);
+		TEST_CHECK(value.LoadRelaxed() == 137);
+	}
+
+	static void
+	UnitTestAtomicAdd()
+	{
+		mg::common::Atomic<int> value(123);
+		value.AddRelaxed(1);
+		TEST_CHECK(value.LoadRelaxed() == 124);
+		value.AddAcqRel(2);
+		TEST_CHECK(value.LoadRelaxed() == 126);
+		value.AddRelease(3);
+		TEST_CHECK(value.LoadRelaxed() == 129);
+		value.Add(4);
+		TEST_CHECK(value.LoadRelaxed() == 133);
+	}
+
+	static void
+	UnitTestAtomicFetchSub()
+	{
+		mg::common::Atomic<int> value(123);
+		TEST_CHECK(value.FetchSubRelaxed(2) == 123);
+		TEST_CHECK(value.FetchSubAcqRel(3) == 121);
+		TEST_CHECK(value.FetchSubRelease(4) == 118);
+		TEST_CHECK(value.FetchSub(5) == 114);
+		TEST_CHECK(value.LoadRelaxed() == 109);
+	}
+
+	static void
+	UnitTestAtomicSubFetch()
+	{
+		mg::common::Atomic<int> value(123);
+		TEST_CHECK(value.SubFetchRelaxed(2) == 121);
+		TEST_CHECK(value.SubFetchAcqRel(3) == 118);
+		TEST_CHECK(value.SubFetchRelease(4) == 114);
+		TEST_CHECK(value.SubFetch(5) == 109);
+		TEST_CHECK(value.LoadRelaxed() == 109);
+	}
+
+	static void
+	UnitTestAtomicSub()
+	{
+		mg::common::Atomic<int> value(123);
+		value.SubRelaxed(1);
+		TEST_CHECK(value.LoadRelaxed() == 122);
+		value.SubAcqRel(2);
+		TEST_CHECK(value.LoadRelaxed() == 120);
+		value.SubRelease(3);
+		TEST_CHECK(value.LoadRelaxed() == 117);
+		value.Sub(4);
+		TEST_CHECK(value.LoadRelaxed() == 113);
+	}
+
+	static void
+	UnitTestAtomicIncrement()
+	{
+		mg::common::Atomic<int> value(123);
+		value.IncrementRelaxed();
+		TEST_CHECK(value.LoadRelaxed() == 124);
+		value.IncrementAcqRel();
+		TEST_CHECK(value.LoadRelaxed() == 125);
+		value.IncrementRelease();
+		TEST_CHECK(value.LoadRelaxed() == 126);
+		value.Increment();;
+		TEST_CHECK(value.LoadRelaxed() == 127);
+	}
+
+	static void
+	UnitTestAtomicFetchIncrement()
+	{
+		mg::common::Atomic<int> value(123);
+		TEST_CHECK(value.FetchIncrementRelaxed() == 123);
+		TEST_CHECK(value.FetchIncrementAcqRel() == 124);
+		TEST_CHECK(value.FetchIncrementRelease() == 125);
+		TEST_CHECK(value.FetchIncrement() == 126);
+		TEST_CHECK(value.LoadRelaxed() == 127);
+	}
+
+	static void
+	UnitTestAtomicIncrementFetch()
+	{
+		mg::common::Atomic<int> value(123);
+		TEST_CHECK(value.IncrementFetchRelaxed() == 124);
+		TEST_CHECK(value.IncrementFetchAcqRel() == 125);
+		TEST_CHECK(value.IncrementFetchRelease() == 126);
+		TEST_CHECK(value.IncrementFetch() == 127);
+		TEST_CHECK(value.LoadRelaxed() == 127);
+	}
+
+	static void
+	UnitTestAtomicDecrement()
+	{
+		mg::common::Atomic<int> value(123);
+		value.DecrementRelaxed();
+		TEST_CHECK(value.LoadRelaxed() == 122);
+		value.DecrementAcqRel();
+		TEST_CHECK(value.LoadRelaxed() == 121);
+		value.DecrementRelease();
+		TEST_CHECK(value.LoadRelaxed() == 120);
+		value.Decrement();;
+		TEST_CHECK(value.LoadRelaxed() == 119);
+	}
+
+	static void
+	UnitTestAtomicFetchDecrement()
+	{
+		mg::common::Atomic<int> value(123);
+		TEST_CHECK(value.FetchDecrementRelaxed() == 123);
+		TEST_CHECK(value.FetchDecrementAcqRel() == 122);
+		TEST_CHECK(value.FetchDecrementRelease() == 121);
+		TEST_CHECK(value.FetchDecrement() == 120);
+		TEST_CHECK(value.LoadRelaxed() == 119);
+	}
+
+	static void
+	UnitTestAtomicDecrementFetch()
+	{
+		mg::common::Atomic<int> value(123);
+		TEST_CHECK(value.DecrementFetchRelaxed() == 122);
+		TEST_CHECK(value.DecrementFetchAcqRel() == 121);
+		TEST_CHECK(value.DecrementFetchRelease() == 120);
+		TEST_CHECK(value.DecrementFetch() == 119);
+		TEST_CHECK(value.LoadRelaxed() == 119);
+	}
+
+	static void
+	UnitTestAtomicCmpExchgWeak()
+	{
+		mg::common::Atomic<int> value(123);
+		int expected = 100;
+		TEST_CHECK(!value.CmpExchgWeakAcquire(expected, 124));
+		TEST_CHECK(value.LoadRelaxed() == 123);
+		TEST_CHECK(expected == 123);
+
+		TEST_CHECK(value.CmpExchgWeakAcquire(expected, 124));
+		TEST_CHECK(value.LoadRelaxed() == 124);
+		TEST_CHECK(expected == 123);
+
+		value.StoreRelaxed(123);
+		expected = 100;
+		TEST_CHECK(!value.CmpExchgWeakAcqRel(expected, 124));
+		TEST_CHECK(value.LoadRelaxed() == 123);
+		TEST_CHECK(expected == 123);
+
+		TEST_CHECK(value.CmpExchgWeakAcqRel(expected, 124));
+		TEST_CHECK(value.LoadRelaxed() == 124);
+		TEST_CHECK(expected == 123);
+
+		value.StoreRelaxed(123);
+		expected = 100;
+		TEST_CHECK(!value.CmpExchgWeakRelease(expected, 124));
+		TEST_CHECK(value.LoadRelaxed() == 123);
+		TEST_CHECK(expected == 123);
+
+		TEST_CHECK(value.CmpExchgWeakRelease(expected, 124));
+		TEST_CHECK(value.LoadRelaxed() == 124);
+		TEST_CHECK(expected == 123);
+
+		value.StoreRelaxed(123);
+		expected = 100;
+		TEST_CHECK(!value.CmpExchgWeak(expected, 124));
+		TEST_CHECK(value.LoadRelaxed() == 123);
+		TEST_CHECK(expected == 123);
+
+		TEST_CHECK(value.CmpExchgWeak(expected, 124));
+		TEST_CHECK(value.LoadRelaxed() == 124);
+		TEST_CHECK(expected == 123);
+	}
+
+	static void
+	UnitTestAtomicCmpExchgStrong()
+	{
+		mg::common::Atomic<int> value(123);
+		int expected = 100;
+		TEST_CHECK(!value.CmpExchgStrongAcquire(expected, 124));
+		TEST_CHECK(value.LoadRelaxed() == 123);
+		TEST_CHECK(expected == 123);
+
+		TEST_CHECK(value.CmpExchgStrongAcquire(expected, 124));
+		TEST_CHECK(value.LoadRelaxed() == 124);
+		TEST_CHECK(expected == 123);
+
+		value.StoreRelaxed(123);
+		expected = 100;
+		TEST_CHECK(!value.CmpExchgStrongAcqRel(expected, 124));
+		TEST_CHECK(value.LoadRelaxed() == 123);
+		TEST_CHECK(expected == 123);
+
+		TEST_CHECK(value.CmpExchgStrongAcqRel(expected, 124));
+		TEST_CHECK(value.LoadRelaxed() == 124);
+		TEST_CHECK(expected == 123);
+
+		value.StoreRelaxed(123);
+		expected = 100;
+		TEST_CHECK(!value.CmpExchgStrongRelease(expected, 124));
+		TEST_CHECK(value.LoadRelaxed() == 123);
+		TEST_CHECK(expected == 123);
+
+		TEST_CHECK(value.CmpExchgStrongRelease(expected, 124));
+		TEST_CHECK(value.LoadRelaxed() == 124);
+		TEST_CHECK(expected == 123);
+
+		value.StoreRelaxed(123);
+		expected = 100;
+		TEST_CHECK(!value.CmpExchgStrong(expected, 124));
+		TEST_CHECK(value.LoadRelaxed() == 123);
+		TEST_CHECK(expected == 123);
+
+		TEST_CHECK(value.CmpExchgStrong(expected, 124));
+		TEST_CHECK(value.LoadRelaxed() == 124);
+		TEST_CHECK(expected == 123);
+	}
+
+	static void
+	UnitTestAtomicBool()
+	{
+		mg::common::Atomic<bool> value(true);
+		TEST_CHECK(value.LoadRelaxed());
+
+		value.StoreRelaxed(false);
+		TEST_CHECK(!value.LoadRelaxed());
+
+		TEST_CHECK(!value.ExchangeRelaxed(true));
+		TEST_CHECK(value.LoadRelaxed());
+
+		bool expected = false;
+		TEST_CHECK(!value.CmpExchgWeakRelaxed(expected, false));
+		TEST_CHECK(expected);
+		TEST_CHECK(value.LoadRelaxed());
+
+		TEST_CHECK(value.CmpExchgWeakRelaxed(expected, false));
+		TEST_CHECK(expected);
+		TEST_CHECK(!value.LoadRelaxed());
+
+		expected = false;
+		value.StoreRelaxed(true);
+		TEST_CHECK(!value.CmpExchgStrongRelaxed(expected, false));
+		TEST_CHECK(expected);
+		TEST_CHECK(value.LoadRelaxed());
+
+		TEST_CHECK(value.CmpExchgStrongRelaxed(expected, false));
+		TEST_CHECK(expected);
+		TEST_CHECK(!value.LoadRelaxed());
+	}
+
+	static void
+	UnitTestAtomicPtr()
+	{
+		int i1, i2;
+		void* val1 = (void*)&i1;
+		void* val2 = (void*)&i2;
+
+		mg::common::Atomic<void*> value(val1);
+		TEST_CHECK(value.LoadRelaxed() == val1);
+
+		value.StoreRelaxed(val2);
+		TEST_CHECK(value.LoadRelaxed() == val2);
+
+		TEST_CHECK(value.ExchangeRelaxed(val1) == val2);
+		TEST_CHECK(value.LoadRelaxed() == val1);
+
+		void* expected = val2;
+		TEST_CHECK(!value.CmpExchgWeakRelaxed(expected, val2));
+		TEST_CHECK(expected == val1);
+		TEST_CHECK(value.LoadRelaxed() == val1);
+
+		TEST_CHECK(value.CmpExchgWeakRelaxed(expected, val2));
+		TEST_CHECK(expected == val1);
+		TEST_CHECK(value.LoadRelaxed() == val2);
+
+		expected = val2;
+		value.StoreRelaxed(val1);
+		TEST_CHECK(!value.CmpExchgStrongRelaxed(expected, val2));
+		TEST_CHECK(expected == val1);
+		TEST_CHECK(value.LoadRelaxed() == val1);
+
+		TEST_CHECK(value.CmpExchgStrongRelaxed(expected, val2));
+		TEST_CHECK(expected == val1);
+		TEST_CHECK(value.LoadRelaxed() == val2);
 	}
 
 	void
@@ -134,13 +361,26 @@ namespace unittests {
 	{
 		TestSuiteGuard suite("Atomic");
 
-		UnitTestAtomicIncrement();
-		UnitTestAtomicDecrement();
-		UnitTestAtomicExchange();
-		UnitTestAtomicCompareExchange();
-		UnitTestAtomicAdd();
-		UnitTestAtomicFlag();
+		UnitTestAtomicConstructor();
 		UnitTestAtomicLoad();
+		UnitTestAtomicStore();
+		UnitTestAtomicExchange();
+		UnitTestAtomicFetchAdd();
+		UnitTestAtomicAddFetch();
+		UnitTestAtomicAdd();
+		UnitTestAtomicFetchSub();
+		UnitTestAtomicSubFetch();
+		UnitTestAtomicSub();
+		UnitTestAtomicIncrement();
+		UnitTestAtomicFetchIncrement();
+		UnitTestAtomicIncrementFetch();
+		UnitTestAtomicDecrement();
+		UnitTestAtomicFetchDecrement();
+		UnitTestAtomicDecrementFetch();
+		UnitTestAtomicCmpExchgWeak();
+		UnitTestAtomicCmpExchgStrong();
+		UnitTestAtomicBool();
+		UnitTestAtomicPtr();
 	}
 
 }
